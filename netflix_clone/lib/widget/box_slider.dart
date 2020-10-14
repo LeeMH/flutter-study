3b25f563639f1a8bcd4b52aa4ce335c26b3c636a
mhlee <jigul.sw@gmail.com>
2020-10-14 16:59:33 +0900

    detail screen 추가

--- netflix_clone/lib/widget/box_slider.dart
+++ netflix_clone/lib/widget/box_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/screen/detail_screen.dart';
 
 class BoxSlider extends StatelessWidget {
   final List<Movie> movies;
@@ -18,7 +19,7 @@ class BoxSlider extends StatelessWidget {
             height: 120,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: makeBoxImages(movies),
+              children: makeBoxImages(context, movies),
             ),
           ),
         ],
@@ -27,13 +28,23 @@ class BoxSlider extends StatelessWidget {
   }
 }
 
-List<Widget> makeBoxImages(List<Movie> movies) {
+List<Widget> makeBoxImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
 
   for (var ii = 0; ii < movies.length; ii++) {
     results.add(
       InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<Null>(
+                fullscreenDialog: true,
+                builder: (BuildContext context) {
+                  return DetailScreen(
+                    movie: movies[ii],
+                  );
+                }),
+          );
+        },
         child: Container(
           padding: EdgeInsets.only(right: 10),
           child: Align(
