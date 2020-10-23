e4ccdc05db1c41aef0ba18f88467b2d2f3ba425c
mhlee <lee.munhaeng@gmail.com>
2020-10-23 18:12:36 +0900

    홈화면 GridView -> AsymmetricView로 변경

--- codelab_mdc/lib/home.dart
+++ codelab_mdc/lib/home.dart
@@ -17,6 +17,8 @@ import 'package:intl/intl.dart';
 import 'model/products_repository.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
+import 'model/products_repository.dart';
+import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
@@ -58,11 +60,8 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(context),
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),
       ),
       resizeToAvoidBottomInset: false,
     );
