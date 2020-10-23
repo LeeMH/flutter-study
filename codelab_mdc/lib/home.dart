9a867c3345f61c0ff422b2fd37842664464f8980
mhlee <lee.munhaeng@gmail.com>
2020-10-23 17:37:29 +0900

    상품 리스트 텍스트 테마 적용

--- codelab_mdc/lib/home.dart
+++ codelab_mdc/lib/home.dart
@@ -83,7 +83,7 @@ class HomePage extends StatelessWidget {
       return Card(
         clipBehavior: Clip.antiAlias,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             AspectRatio(
               aspectRatio: 18 / 11,
@@ -97,17 +97,20 @@ class HomePage extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      product.name,
-                      style: theme.textTheme.headline6,
+                      product == null ? '' : product.name,
+                      style: theme.textTheme.button,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
                       maxLines: 1,
                     ),
                     SizedBox(height: 8.0),
                     Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.subtitle2,
+                      product == null ? '' : formatter.format(product.price),
+                      style: theme.textTheme.caption,
                     )
                   ],
                 ),
