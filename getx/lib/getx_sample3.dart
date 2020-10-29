82b899c2e01ce6b2f4e6d3bdc344396aedb729aa
mhlee <lee.munhaeng@gmail.com>
2020-10-29 12:51:00 +0900

    Controller 바인딩 예제 추가

---
+++ getx/lib/getx_sample3.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(GetMaterialApp(
+    initialRoute: '/home',
+    getPages: [
+      GetPage(name: '/home', page: () => HomeView(), binding: HomeBinding()),
+    ],
+  ));
+}
+
+class HomeView extends GetView<HomeController> {
+  @override
+  Widget build(context) => Scaffold(
+      appBar: AppBar(title: Text("counter")),
+      body: Center(
+        child: Obx(() => Text("${controller.count}")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: controller.increment,
+      ));
+}
+
+mixin Controller {}
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => HomeController());
+  }
+}
+
+class HomeController extends GetxController {
+  var count = 0.obs;
+  void increment() => count++;
+}
